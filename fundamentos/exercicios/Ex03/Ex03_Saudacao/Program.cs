4cf5a82de873e269f5bee2cc88d8df9391614128
PedroHFG <pedrohfidg@gmail.com>
2025-04-05 12:26:36 -0300

    Exercicio 03

---
+++ fundamentos/exercicios/Ex03/Ex03_Saudacao/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercicio03;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Qual o seu nome? ");
+        string nome = Console.ReadLine();
+        Console.Write("Qual a sua idade? ");
+        int idade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"\nOlá {nome}! Você tem {idade} anos.");
+    }
+}
